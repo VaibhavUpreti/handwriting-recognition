4cd4eda9b8ed710dc1bfb4f3c07bb7a1ecbe0a43
VaibhavUpreti <vaibhav.upreti16@gmail.com>
2023-04-12 20:31:52 +0530

    feat: missing files

--- app.py
+++ app.py
@@ -55,9 +55,9 @@ def predict(image_path):
 
 class FilePaths:
     """Filenames and paths to data."""
-    fn_char_list = '../model/charList.txt'
-    fn_summary = '../model/summary.json'
-    fn_corpus = '../data/corpus.txt'
+    fn_char_list = 'model/charList.txt'
+    fn_summary = 'model/summary.json'
+    fn_corpus = 'data/corpus.txt'
 
 
 def char_list_from_file() -> List[str]:
